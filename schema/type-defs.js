363e291a4824eaa23693309ea52b81ea906f438e
nngonguyen <ngonguyen.tronhouse@gmail.com>
2023-11-08 21:13:04 +0700

    mutation

---
+++ schema/type-defs.js
@@ -0,0 +1,55 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+
+    type  User {
+        id: ID!
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality!
+        friends: [User]
+        favoriteMovies: [Movie]
+    }
+
+    type Movie {
+        id: ID!
+        movieName: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!    
+    }
+
+    type Query {
+        users: [User!]!
+        user(id:ID!): User!
+        movies: [Movie!]!
+        movie(movieName: String!): Movie!
+    }
+
+    input CreateUserInput {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = BRAZIL
+    }
+
+    input UpdateUsernameInput {
+        id: ID!
+        newUsername: String!
+    }
+
+    type Mutation {
+        createUser(input: CreateUserInput!): User
+        updateUsername(input: UpdateUsernameInput!): User
+    }
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
+    }
+`
+
+module.exports = { typeDefs }
